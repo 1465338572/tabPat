e6aa4db54ced8142de5418ee043264faa2cda5d2
xxb <1465338572@qq.com>
2024-07-21 23:34:52 +0800

    无用测试信删除

--- src/test/java/com/example/tabpat/TabPatApplicationTests.java
+++ src/test/java/com/example/tabpat/TabPatApplicationTests.java
@@ -12,17 +12,4 @@ import java.net.URLConnection;
 @SpringBootTest
 class TabPatApplicationTests {
 
-    @Test
-    void contextLoads() throws IOException {
-        URL url = new URL("https://google.com");
-        URLConnection connection = url.openConnection();
-        connection.connect();
-        InputStream inputStream = connection.getInputStream();
-        byte[] bytes = new byte[1024];
-        while (inputStream.read(bytes) >= 0) {
-            System.out.println(new String(bytes));
-        }
-
-    }
-
 }
